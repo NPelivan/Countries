7e9b515518b4dfcda87ad0b92f86c20e9fa01b8b
Nikolina Pelivan <nikolina_pelivan@hotmail.com>
2020-07-13 22:03:07 +0200

    work in progress

---
+++ src/components/card.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Card(props) {
+  const { clickHandler, country } = props;
+  const { name, population, region, capital, flag } = country;
+  return (
+    <div className="country-card" onClick={clickHandler}>
+      <img src={flag} alt={`${name} flag`}></img>
+      <div className="country-card-body">
+        <div className="country-card-name">{name}</div>
+
+        <div className="country-card-body-row">
+          <span className="country-card-body-row-title">Population:</span>
+          <span>{population.toLocaleString()}</span>
+        </div>
+
+        <div className="country-card-body-row">
+          <span className="country-card-body-row-title">Region:</span>
+          <span>{region.toLocaleString()}</span>
+        </div>
+
+        <div className="country-card-body-row">
+          <span className="country-card-body-row-title">Capital:</span>
+          <span>{capital.toLocaleString()}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import * as action from "../store/actions/index";
+import CountryCard from "../components/card";
+import FilterRow from "../components/filterRow";
 
-export default function HomePage() {
-  return <div>Home Page</div>;
+export default function HomePage(props) {
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.filteredCountries);
+
+  useEffect(() => {
+    dispatch(action.fetchCountries());
+  }, [dispatch]);
+
+  const countryClickedHandler = (alpha3code) => {
+    console.log("alpha3code");
+  };
+
+  return (
+    <>
+      <FilterRow />
+      {countries.length > 0 && (
+        <div className="countries-wrapper">
+          {countries.map((c) => (
+            <CountryCard
+              clickHanlder={() => countryClickedHandler(c.alpha3code)}
+              key={c.alpha3code}
+              country={c}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
